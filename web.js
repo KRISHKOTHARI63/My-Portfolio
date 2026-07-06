766c2a17751790508864f89011b370b84c53ddde
KRISH <kotharigkrish@gmail.com>
2026-07-06 19:10:07 +0530

    Initial portfolio website

---
+++ web.js
@@ -0,0 +1,131 @@
+// function openCertificate(image){
+
+//     document.getElementById("popupImage").src = image;
+
+//     document.getElementById("popup").style.display = "flex";
+// }
+
+// function closeCertificate(){
+
+//     document.getElementById("popup").style.display = "none";
+// }
+
+function openMetaCertificate(image){
+
+    document.getElementById("popupImage0").src = image;
+
+    document.getElementById("popup0").style.display = "flex";
+}
+function openMicrosoftCertificate(image){
+
+    document.getElementById("popupImage1").src = image;
+
+    document.getElementById("popup1").style.display = "flex";
+}
+function openGoogleCertificate(image){
+
+    document.getElementById("popupImage2").src = image;
+
+    document.getElementById("popup2").style.display = "flex";
+}
+function openIBMCertificate(image){
+
+    document.getElementById("popupImage3").src = image;
+
+    document.getElementById("popup3").style.display = "flex";
+}
+function openAWSCertificate(image){
+
+    document.getElementById("popupImage4").src = image;
+
+    document.getElementById("popup4").style.display = "flex";
+}
+function openSimplilearnCertificate(image){
+
+    document.getElementById("popupImage5").src = image;
+
+    document.getElementById("popup5").style.display = "flex";
+}
+function openCodeWithHarryCertificate(image){
+
+    document.getElementById("popupImage6").src = image;
+
+    document.getElementById("popup6").style.display = "flex";
+}
+function openUdemyCertificate(image){
+
+    document.getElementById("popupImage7").src = image;
+
+    document.getElementById("popup7").style.display = "flex";
+}
+function openEliteForumsCertificate(image){
+
+    document.getElementById("popupImage8").src = image;
+
+    document.getElementById("popup8").style.display = "flex";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function closeCertificate1(){
+
+    document.getElementById("popup0").style.display = "none";
+}
+
+function closeCertificate2(){
+
+    document.getElementById("popup1").style.display = "none";
+}
+function closeCertificate3(){
+
+    document.getElementById("popup2").style.display = "none";
+}
+function closeCertificate4(){
+
+    document.getElementById("popup3").style.display = "none";
+}
+function closeCertificate5(){
+
+    document.getElementById("popup4").style.display = "none";
+}
+function closeCertificate6(){
+
+    document.getElementById("popup5").style.display = "none";
+}
+function closeCertificate7(){
+
+    document.getElementById("popup6").style.display = "none";
+}
+function closeCertificate8(){
+
+    document.getElementById("popup7").style.display = "none";
+}
+function closeCertificate9(){
+
+    document.getElementById("popup8").style.display = "none";
+}
+
+
+
+
+
+
+
+
